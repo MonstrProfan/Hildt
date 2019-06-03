ef0c93b5cea007d4edc6250afd6bdbeb4074c3fa
Petr Mihkin <petrmihkin@mail.ru>
2019-06-03 15:44:18 +0300

    расчет колличества жильцов исходя из нормирования площади на 1-го человека

---
+++ Dom/ParametrDom.cs
@@ -0,0 +1,57 @@
+﻿using static System.Console;
+
+namespace Dom
+{
+    class ParametrDom
+    {
+        int a, b, c;
+        string s;
+
+        public int A
+        {
+            get => this.a; set => this.a = value;
+        }
+        public int B
+        {
+            get => this.b; set => this.b = value;
+        }
+        public int C
+        {
+            get => this.c; set => this.c = value;
+        }
+        public string S
+        {
+            get => this.s; set => this.s = value;
+        }
+                     
+        /// <summary>
+        /// расчет приходящейся площади на одного человека
+        /// </summary>
+        /// <returns></returns>
+        public int PlOdChel()
+        {
+            return B / C;
+        }
+
+        /// <summary>
+        /// расчет колличества возможных жильцов исходя из нормирования площади
+        /// </summary>
+        /// <param name="B"></param>
+        /// <returns></returns>
+        public int KolChel(int B)
+        {
+            return B / 12;
+        }
+
+        public void Oth()
+        {
+            WriteLine($"При полученных данных: ");
+            WriteLine($"Объект: - {S}");
+            WriteLine($"Имеим: - Площадь на 1-го чел. = {PlOdChel()}");
+            WriteLine($"Исхдя из общей площади здания = {B}m2 и ноpме на 1-го человека 12 м2 в здании может быть {KolChel(B)} человек");
+
+            ReadKey();
+        }
+    }
+}
+ 
---
+++ Dom/StartDom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dom
+{
+    class StartDom
+    {
+        static void Main()
+        {
+            ParametrDom haus = new ParametrDom();
+
+            Write("Наименование - .....");
+            haus.S = ReadLine();
+            Write("Колличество этажей - .....");
+            haus.A = Convert.ToInt32(Console.ReadLine());
+            Write("Oбщая площадь здания - .....");
+            haus.B = Convert.ToInt32(Console.ReadLine());
+            Write("Колличество жильцов - .....");
+            haus.C = Convert.ToInt32(Console.ReadLine());
+
+            haus.Oth();
+        }
+    }
+}
